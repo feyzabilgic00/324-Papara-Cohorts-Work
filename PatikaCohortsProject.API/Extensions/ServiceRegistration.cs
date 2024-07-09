36ff45493599d3522c4f9d4a77ae7bf36c5497c8
feyzabilgi <150264038+feyzabilgi@users.noreply.github.com>
2024-07-09 10:09:07 +0300

    The extensions for IOC have been added: A method named AddServices has been added to the ServiceRegistration class.

--- PatikaCohortsProject.API/Extensions/ServiceRegistration.cs
+++ PatikaCohortsProject.API/Extensions/ServiceRegistration.cs
@@ -1,21 +1,21 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using PatikaCohortsProject.API.Base.Repositories;
 using PatikaCohortsProject.API.Context;
+using PatikaCohortsProject.API.Model;
 using PatikaCohortsProject.API.Repositories.Product;
+using PatikaCohortsProject.API.Services;
 
-namespace PatikaCohortsProject.API.Utils;
+namespace PatikaCohortsProject.API.Extensions;
 
 public static class ServiceRegistration
 {
-    //public static void AddServices(this IServiceCollection services, IConfiguration configuration)
-    //{
-    //    services.AddDbContext<AppDbContext>(options => options.UseSqlServer(configuration.GetSection("ConnectionDB").Value));
-    //    services.AddScoped<IProductReadRepository, ProductReadRepository>();
-    //    services.AddScoped<IProductWriteRepository, ProductWriteRepository>();
-    //}
     public static void AddServices(this IServiceCollection services)
     {
+        services.AddScoped<IGenericReadRepository<ProductEntity>, GenericReadRepository<ProductEntity>>();
+        services.AddScoped<IGenericWriteRepository<ProductEntity>, GenericWriteRepository<ProductEntity>>();
         services.AddScoped<IProductReadRepository, ProductReadRepository>();
         services.AddScoped<IProductWriteRepository, ProductWriteRepository>();
+        services.AddScoped<IProductService, ProductService>();
     }
 }
--- PatikaCohortsProject.API/Program.cs
+++ PatikaCohortsProject.API/Program.cs
@@ -1,15 +1,9 @@
-using FluentValidation;
 using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json.Serialization;
-using PatikaCohortsProject.API.Base;
-using PatikaCohortsProject.API.Base.Repositories;
 using PatikaCohortsProject.API.Context;
+using PatikaCohortsProject.API.Extensions;
 using PatikaCohortsProject.API.Model;
-using PatikaCohortsProject.API.Repositories.Product;
-using PatikaCohortsProject.API.Services;
-using PatikaCohortsProject.API.Validators;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -27,11 +21,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("MsSqlConnection"));
 });
 
-builder.Services.AddScoped<IGenericReadRepository<ProductEntity>, GenericReadRepository<ProductEntity>>();
-builder.Services.AddScoped<IGenericWriteRepository<ProductEntity>, GenericWriteRepository<ProductEntity>>();
-builder.Services.AddScoped<IProductReadRepository, ProductReadRepository>();
-builder.Services.AddScoped<IProductWriteRepository, ProductWriteRepository >();
-builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddServices();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
